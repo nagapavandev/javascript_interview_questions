1241c9e19d26f47e86c0e0c2a1baccf6ab6e9112
nagapavan.tanguturi <nagapavan.tanguturi@taodigitalusa.com>
2024-08-10 11:58:16 +0530

    feat : example of console table

---
+++ 3.console.table.js
@@ -0,0 +1,12 @@
+const users = [
+  { name: 'pavan', age: 33, occupation: 'software developer' },
+  { name: 'basha', age: 26, occupation: 'Project Manager' },
+]
+
+console.table(users);
+
+const numericArray = [
+  0, 1, 2, 3
+]
+
+console.table(numericArray);
